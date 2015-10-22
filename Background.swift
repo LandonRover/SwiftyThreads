6cbb45f0eeed1b16121ad5c7c7e205b8e9b7ac67
Andrew Robinson <RobinsonSoftwareDev@gmail.com>
2015-10-22 16:00:58 -0400

    Create Background.swift

---
+++ Background.swift
@@ -0,0 +1,48 @@
+//
+//  Backround.swift
+//  Blur
+//
+//  Created by Andrew Robinson on 10/22/15.
+//  Copyright © 2015 Andrew Robinson. All rights reserved.
+//
+
+import Foundation
+
+class Background {
+  
+  private var done = false {
+    didSet {
+      if done {
+        taskInterval = nil
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+          completion?()
+        })
+      }
+    }
+  }
+  
+  private var taskInterval: (() -> ())?
+  private var completion: (() -> ())?
+  
+  init(task: () -> ()) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { () -> Void in
+      task()
+      self.done = true
+    }
+  }
+  
+  func afterInterval(interval: NSTimeInterval, closure: () -> ()) -> Background {
+    taskInterval = closure
+    let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(interval * Double(NSEC_PER_SEC)))
+    dispatch_after(delayTime, dispatch_get_main_queue()) {
+      taskInterval?()
+    }
+    return self
+  }
+  
+  func completion(closure: () -> ()) -> Background {
+    completion = closure
+    return self
+  }
+  
+}
